3b26692272b59af88f7c5e89d583c0c35cbe4c13
Pauline-Humbert <42661326+Pauline-Humbert@users.noreply.github.com>
2020-11-18 14:31:07 +0100

    initialisation proxy

---
+++ src/main/java/com/ProxyServerCabinetMedicalApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyServerCabinetMedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyServerCabinetMedicalApplication.class, args);
+	}
+
+}
